929c350b9be3177ea11328d82fba8849a3a31fbe
Cat Knight <phoenixrebirth1324@gmail.com>
2022-01-13 10:39:43 -0300

    Minha versão de manipulação do JSON
    Adicionei minha versão de como manipular o json com o read e write junto de um exemplo de como loopar o atributo "transições"

---
+++ operatingJSON.py
@@ -0,0 +1,28 @@
+import json
+import pathlib
+
+# site do tutorial https://www.programiz.com/python-programming/json
+
+path_input = pathlib.Path(pathlib.Path.cwd(), 'test', 'linguagem01.json')
+path_output = pathlib.Path(pathlib.Path.cwd(), 'test', 'linguagem02.json')
+
+with open(path_input) as f:
+    data = json.load(f)
+
+# como loopar as transições
+for item in data['transitions']:
+    print("de " + item['estado_de_saida'] + " para " + item['estado_de_chegada'] + " por " + item['simbolo_de_transicao'])
+
+# Como declarar um json(so por curiosidade
+# person_dict = {"name": "Bob",
+#                "languages": ["English", "Fench"],
+#                "married": True,
+#                "age": 32
+#                }
+
+
+with open(path_output, 'w') as json_file:
+    json.dump(data, json_file, indent=4, sort_keys=True)
+
+# with open(path_output, 'w') as json_file:
+#     json.dump(person_dict, json_file, indent=4, sort_keys=True)
