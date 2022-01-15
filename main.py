6ce002b15aaccec4c8671ce5933d74adf813c158
Cat Knight <phoenixrebirth1324@gmail.com>
2022-01-15 10:12:54 -0300

    Simplificação na chamada
    Não sei muito bem o phyton, mas acho que da de simplificar como chama as funções(achar um jeito de não precisar passar o "self")

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import operating_JSON
 
 if __name__ == "__main__":
     test_module_instantiation = operating_JSON
-    operating_JSON.load_to_memory(test_module_instantiation, "language_01")
+    test_module_instantiation.load_to_memory(test_module_instantiation, "language_01")
     print(test_module_instantiation.data)
