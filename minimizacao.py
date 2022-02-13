12b85474f7c80ca4552fcdb3255a434e7335feae
LeonardoVieiraNunes <devleonardonunes@gmail.com>
2022-02-13 17:52:57 -0300

    ajustes caso o automato já seja minimo

--- minimizacao.py
+++ minimizacao.py
@@ -15,11 +15,13 @@ class Minimizacao(MyOperatingJSON):
         self.automato = automato
         self.new_automata = MyOperatingJSON()
 
-    def construirTabelaEstados(self):
+    def minimizar(self):
         nroTemp = 1
         for i in self.estados[1:]:
             self.tabelaEstados[i] = [None] * nroTemp
             nroTemp += 1
+
+        self.marcarEstados()
 
     def marcarEstados(self):
         for estado in self.tabelaEstados.keys():
@@ -27,6 +29,8 @@ class Minimizacao(MyOperatingJSON):
                 if (estado in self.finais and self.estados[i] not in self.finais) or (
                         estado not in self.finais and self.estados[i] in self.finais):
                     self.tabelaEstados[estado][i] = 1
+
+        self.verificarNaoMarcados()
 
     def isMarcado(self, stateToP, stateToQ):
         if stateToP in self.tabelaEstados:
@@ -62,6 +66,8 @@ class Minimizacao(MyOperatingJSON):
             if not self.repetirMarcacao:
                 break
 
+        self.gerarNaoMarcados()
+
     def combinarNaoMarcados(self, head, tail):
         originalHead = head
         for item in tail:
@@ -77,14 +83,20 @@ class Minimizacao(MyOperatingJSON):
         else:
             self.estadosOtimizados += tail
 
+        self.preencherNaoCombinados()
+
     def setEstadosNovoAutomato(self):
         for estadoOtimizado in self.estadosOtimizados:
             self.new_automata.add_state('_'.join(sorted(estadoOtimizado)))
+
+        self.setTransicoesNovoAutomato()
 
     def preencherNaoCombinados(self):
         for estado in self.estados:
             if not list(filter(lambda c: estado in c, self.estadosOtimizados)):
                 self.estadosOtimizados.append({estado})
+
+        self.setEstadosNovoAutomato()
 
     def gerarNaoMarcados(self):
         for estado in self.tabelaEstados.keys():
@@ -92,7 +104,14 @@ class Minimizacao(MyOperatingJSON):
                 if self.tabelaEstados[estado][i] is None:
                     self.estadosCombinados.append({estado, self.estados[i]})
 
-        self.combinarNaoMarcados(self.estadosCombinados[0], self.estadosCombinados[1:])
+        if self.estadosCombinados:
+            if len(self.estadosCombinados) > 1:
+                self.combinarNaoMarcados(self.estadosCombinados[0], self.estadosCombinados[1:])
+            else:
+                self.combinarNaoMarcados(self.estadosCombinados[0], [])
+        else:
+            print("O automato já está minimizado")
+            self.new_automata = self.automato
 
     def setTransicoesNovoAutomato(self):
         for otimizados in self.estadosOtimizados:
@@ -103,10 +122,14 @@ class Minimizacao(MyOperatingJSON):
                 stateTo = next(filter(lambda e: transitions[symbol] in e, self.estadosOtimizados))
                 self.new_automata.add_transition('_'.join(sorted(otimizados)), '_'.join(sorted(stateTo)), symbol)
 
+        self.setEstadoInicialNovoAutomato()
+
     def setEstadosFinaisNovoAutomato(self):
         for estado in self.estadosOtimizados:
             if estado.intersection(self.finais):
                 self.new_automata.add_final('_'.join(sorted(estado)))
+
+        self.setSimbolosNovoAutomato()
 
     def setEstadoInicialNovoAutomato(self):
         for estado in self.estadosOtimizados:
@@ -114,18 +137,7 @@ class Minimizacao(MyOperatingJSON):
                 self.new_automata.set_initial('_'.join(sorted(estado)))
                 break
 
+        self.setEstadosFinaisNovoAutomato()
+
     def setSimbolosNovoAutomato(self):
         self.new_automata.data['symbols'] = self.simbolos
-
-    def minimizar(self):
-        self.construirTabelaEstados()
-        self.marcarEstados()
-        self.verificarNaoMarcados()
-        self.gerarNaoMarcados()
-        self.preencherNaoCombinados()
-        self.setEstadosNovoAutomato()
-        self.setTransicoesNovoAutomato()
-        self.setEstadoInicialNovoAutomato()
-        self.setEstadosFinaisNovoAutomato()
-        self.setSimbolosNovoAutomato()
-
