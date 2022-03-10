1f62dab759daeba991df6718d60c1831fc1edf36
LeonardoVieiraNunes <devleonardonunes@gmail.com>
2022-03-09 22:39:48 -0300

    add eliminação de recursão a esquerda

--- GLC.py
+++ GLC.py
@@ -22,7 +22,53 @@ class GLC:
             else:
                 self.producoes[naoTerminal] += [producao]
 
+    def eliminar_recursoes(self):
+        naoTerminais = self.naoTerminais
+        for i in range(len(naoTerminais)):
+            producoesI: list = self.producoes[naoTerminais[i]]
+            for j in range(i):
+
+                for prodI in producoesI:
+                    alpha = None
+                    if len(prodI) > 1:
+                        alpha = prodI[1:]
+
+                    if prodI[0] == naoTerminais[j]:
+                        producoesI.remove(prodI)
+                        producoesJ = self.producoes[naoTerminais[j]]
+                        for prodJ in producoesJ:
+                            producoesI.append(prodJ + alpha)
+            hasRecEsquerda = len(list(filter(lambda p: p[0] == naoTerminais[i], producoesI))) > 0
+            if hasRecEsquerda:
+                novoNaoTerminal = f"{naoTerminais[i]}'"
+
+                self.producoes[novoNaoTerminal] = list()
+                producoesNovoTerminal: list = self.producoes[novoNaoTerminal]
+
+                self.naoTerminais.append(novoNaoTerminal)
+                for k in range(len(producoesI)):
+                    if producoesI[k][0] != naoTerminais[i]:
+                        producoesI[k] += novoNaoTerminal
+                    else:
+                        producoesNovoTerminal.append(producoesI[k][1:] + novoNaoTerminal)
+
+                producoesNovoTerminal.append('&')
+
+                for prod in producoesI:
+                    if prod[0] == naoTerminais[i]:
+                        producoesI.remove(prod)
+
+                pass
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     glc = GLC()
-    glc.load_to_memory('test/rec_esquerda.txt')
+    glc.load_to_memory('test/rec_esquerda_2.txt')
+    glc.eliminar_recursoes()
